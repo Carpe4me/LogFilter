2a9fee5c7ae8e9bb704a8c190c72b25336a3efa5
hexinyu <hexinyu@bigo.sg>
2019-02-02 11:51:21 +0800

    +) FixPopup 支持拖动

--- src/com/legendmohe/tool/view/FixPopup.java
+++ src/com/legendmohe/tool/view/FixPopup.java
@@ -1,10 +1,14 @@
 package com.legendmohe.tool.view;
 
+import java.applet.Applet;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
+import java.awt.Point;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -17,6 +21,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.Popup;
 import javax.swing.PopupFactory;
+import javax.swing.RootPaneContainer;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
@@ -38,6 +43,9 @@ public class FixPopup extends JPanel {
 
     private boolean mIsPinned;
 
+    // popup内部的component，用于drag
+    private Component mInternalPopupComponent;
+
     public FixPopup(String message, int maxWidth) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, BORDER_THICKNESS, true));
@@ -52,6 +60,9 @@ public class FixPopup extends JPanel {
         add(textArea);
 
         MouseAdapter mouseAdapter = new MouseAdapter() {
+            private int mMouseY;
+            private int mMouseX;
+
             @Override
             public void mouseEntered(MouseEvent e) {
                 mIsMouseEntered = true;
@@ -60,6 +71,20 @@ public class FixPopup extends JPanel {
             @Override
             public void mouseExited(MouseEvent e) {
                 mIsMouseEntered = false;
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mMouseX = e.getX();
+                mMouseY = e.getY();
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (mInternalPopupComponent != null) {
+                    Point point = SwingUtilities.convertPoint(FixPopup.this, e.getX(), e.getY(), mInternalPopupComponent.getParent());
+                    mInternalPopupComponent.setLocation(point.x - mMouseX, point.y - mMouseY);
+                }
             }
         };
         addMouseListener(mouseAdapter);
@@ -130,12 +155,29 @@ public class FixPopup extends JPanel {
         hidePopup();
         mInternalPopup = new PopupFactory().getPopup(owner, this, x, y);
         mInternalPopup.show();
+        mInternalPopupComponent = getInnerPopupComponent(owner, x, y);
+    }
+
+    // refer to javax.swing.PopupFactory.MediumWeightPopup.show
+    private Component getInnerPopupComponent(Component owner, int x, int y) {
+        Container parent = owner.getParent();
+        while (!(parent instanceof Window || parent instanceof Applet) &&
+                (parent != null)) {
+            parent = parent.getParent();
+        }
+        if (parent instanceof RootPaneContainer) {
+            parent = ((RootPaneContainer) parent).getLayeredPane();
+            return parent.getComponentAt(x, y);
+        }
+
+        return null;
     }
 
     public void hidePopup() {
         if (mInternalPopup != null) {
             mInternalPopup.hide();
             mInternalPopup = null;
+            mInternalPopupComponent = null;
         }
     }
 }
